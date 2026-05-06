71ce3c7e92a0ef6dd2be96df56c7d4a6d0a73520
latinasontop <chiraykarkera@gmail.com>
2026-05-06 13:33:51 +0530

    Create hashmap.java

---
+++ hashmap.java
@@ -0,0 +1,14 @@
+
+import java.util.HashMap;
+public class Main
+{
+	public static void main(String[] args) {
+	HashMap<Integer,String>m=new HashMap<>();
+		m.put(101,"Alice");
+		m.put(102,"John");
+		System.out.println(m);
+		System.out.println(m.size());
+		m.remove(102);
+	    	System.out.println(m);
+	}
+}
