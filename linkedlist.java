c56d3589bc85c225f6d958df57a9151b1c6933fb
latinasontop <chiraykarkera@gmail.com>
2026-05-06 11:06:37 +0530

    Create linkedlist.java

---
+++ linkedlist.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+public class Main
+{
+	public static void main(String[] args) {
+	    LinkedList<Integer>CSE=new LinkedList<>();
+	    CSE.add(10);
+	    CSE.add(20);
+	    CSE.addFirst(5);
+	    System.out.println(CSE);
+		System.out.println(CSE.get(0));
+	}
+}
